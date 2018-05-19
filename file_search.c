3dd8c2858abfa38e23786cee2aa2a6c2bbc36de0
andrewang123 <andrewang360@gmail.com>
2018-05-19 15:02:39 -0700

    finished function that would validate the path of the starting directory making sure that it start with a / and does not end with a /

--- file_search.c
+++ file_search.c
@@ -6,7 +6,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/time.h>
-float  getTime(struct timeval t0, struct timeval t1);
+#include <string.h>
+void validatePath(char* startDir);
+void searchFile(char* searchTerm, char* startDir);
+float getTime(struct timeval t0, struct timeval t1);
 int main(int argc, char* argv[])
 {
 	struct timeval start;
@@ -18,6 +21,13 @@ int main(int argc, char* argv[])
 		exit(0);
 	} else if (argc == 3)
 	{
+		char* searchTerm = strdup(argv[1]);
+		char* startDir = strdup(argv[2]);
+	
+//		printf("%s\n", searchTerm);
+//		printf("%s\n", startDir);
+	
+		validatePath(startDir);
 		if(gettimeofday(&start, NULL) < 0)
 		{
 			perror("gettimeofday");
@@ -32,11 +42,39 @@ int main(int argc, char* argv[])
 			exit(0);
 		}
 		elapsed = getTime(start,end);
-		printf("Time:  %fms\n", elapsed);
+		printf("Time: %fms\n", elapsed);
 
 	} else {
 		printf("there are more than 3 things in command line\n");
+		exit(0);
 	}
+}
+
+
+
+
+
+void validatePath(char* startDir)
+{
+	// check if start is / make sure does NOT end with /	
+	// if fail exit program
+	printf("entered valid path function\n");
+	printf("%s\n", startDir);
+	if(startDir[0] == '/' && startDir[strlen(startDir) - 1] != '/')
+	{
+		printf("successfully pass test\n");
+		return;
+	} else {
+		printf("Starting directory must start with a / and cannot end with a /\n");
+		exit(0);
+	}
+}
+
+// Recursivly search
+void searchFile(char* searchTerm, char* startDir)
+{
+
+
 }
 
 float getTime(struct timeval t0, struct timeval t1)

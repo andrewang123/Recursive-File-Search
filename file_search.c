e8ecb7474fdafa12014b188865c8bbd4032b0490
andrewang123 <andrewang360@gmail.com>
2018-05-19 10:05:04 -0700

    made a function to calculate the difference between the start and end time, it computes miliseconds

--- file_search.c
+++ file_search.c
@@ -5,20 +5,41 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-
-
+#include <sys/time.h>
+float  getTime(struct timeval t0, struct timeval t1);
 int main(int argc, char* argv[])
 {
-
+	struct timeval start;
+	struct timeval end;
+	float elapsed;
 	if(argc <= 2)
 	{
 		printf("There are less than three  arguments, this program requires at least 3... Exiting program\n");
 		exit(0);
 	} else if (argc == 3)
 	{
-		printf("there are three things in the command line\n");
+		if(gettimeofday(&start, NULL) < 0)
+		{
+			perror("gettimeofday");
+			exit(0);
+		}
+
+		// DO THE RECURSION HERE
+
+		if(gettimeofday(&end, NULL) < 0)
+		{
+			perror("gettimeofday");
+			exit(0);
+		}
+		elapsed = getTime(start,end);
+		printf("Time:  %fms\n", elapsed);
 
 	} else {
 		printf("there are more than 3 things in command line\n");
 	}
+}
+
+float getTime(struct timeval t0, struct timeval t1)
+{
+	return (t1.tv_sec - t0.tv_sec) * 1000.0f + (t1.tv_usec - t0.tv_usec) / 1000.0f;
 }

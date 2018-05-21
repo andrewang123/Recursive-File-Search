6ddc0b5a88649f05c8a89fa812cea501ef43130b
andrewang123 <andrewang360@gmail.com>
2018-05-21 15:27:02 -0700

    finished comment all of the code in main and all of the functions except for the recursive function

--- file_search.c
+++ file_search.c
@@ -14,57 +14,69 @@ void searchFile(char* searchTerm, char* startDir);
 float getTime(struct timeval t0, struct timeval t1);
 int main(int argc, char* argv[])
 {
-	struct timeval start;
+	// Create a struct that will represent the time that has elapsed
+	// timeval two member variables: tv_sec (seconds) and tv_usec (microseconds)
+	struct timeval start; 
 	struct timeval end;
-	float elapsed;
-	if(argc <= 2)
+	float elapsed; // represents the total amount of time elapsed in miliseconds
+	if(argc <= 2) // if the user only types in less than three arguements 
 	{
-		printf("There are less than three  arguments, this program requires at least 3... Exiting program\n");
-		exit(0);
-	} else if (argc == 3)
+		printf("There are less than three arguments, this program requires at least 3... Exiting program\n");
+		exit(0); // exit the program
+	} else if (argc == 3) // if the user types in three arguements
 	{
+		// strdup returns a pointer to a new string which is a duplicate of the parameter passed
+		// it is essentially a strcpy and a malloc built into one
+		// make a deep copy of the buffer, it is like strcpy but dynamically allocated memory		
 		char* searchTerm = strdup(argv[1]);
 		char* startDir = strdup(argv[2]);
-	
-//		printf("%s\n", searchTerm);
-//		printf("%s\n", startDir);
-	
+		// ensure that the path stat with a / and does not end with a / (absolute path)
 		validatePath(startDir);
+		
+		// gets the time of day at the moment and puts it in start
+		// returns a number,0 if success and -1 if failure
+		// NULL is to specifying the time zone
 		if(gettimeofday(&start, NULL) < 0)
 		{
 			perror("gettimeofday");
 			exit(0);
 		}
-
-		// DO THE RECURSION HERE
-		searchFile(searchTerm, startDir);				
+		// recursivly search for the files and directories
+		searchFile(searchTerm, startDir);			
+		
+		// gets the time of day at the moment and puts it in end
+		// returns a number,0 if success and -1 if failure
+		// NULL is to specifying the time zone	
 		if(gettimeofday(&end, NULL) < 0)
 		{
 			perror("gettimeofday");
 			exit(0);
 		}
+		// calculate the amount of miliseconds it took to complete the task and print it out
 		elapsed = getTime(start,end);
 		printf("Time: %fms\n", elapsed);
 
-	} else {
+		// free up the strings allocated in the heap
+		free(searchTerm);
+		free(startDir);
+	} else { // if the user types in more than three arguements
 		printf("there are more than 3 things in command line\n");
-		exit(0);
+		exit(0); // exit the program
 	}
 }
 
-
+// Validates the path that the user specifies in the command line
+// Parameters: the path the user specifies
+// Returns: N/A
 void validatePath(char* startDir)
 {
 	// check if start is / make sure does NOT end with /	
-	// if fail exit program
-//	printf("%s\n", startDir);
 	if(startDir[0] == '/' && startDir[strlen(startDir) - 1] != '/')
 	{
-		//printf("successfully pass test\n");
 		return;
 	} else {
 		printf("Starting directory must start with a / and cannot end with a /\n");
-		exit(0);
+		exit(0); // exit the program
 	}
 }
 
@@ -138,9 +150,13 @@ void searchFile(char* searchTerm, char* startDir)
 			}
 	*/	
 	}
+	// FREE MEMORY
 	closedir(dr);	
 }
 
+// Calculates the amount of time it took in miliseconds between two timevals
+// Parameter: The start and ending time values
+// Returns: difference between the end timeval (2nd param) and the start timeval (1st param)
 float getTime(struct timeval t0, struct timeval t1)
 {
 	return (t1.tv_sec - t0.tv_sec) * 1000.0f + (t1.tv_usec - t0.tv_usec) / 1000.0f;
